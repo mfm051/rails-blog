fdc03446d8b1d6f239da3d4d8895df880429d746
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-07-07 19:13:13 -0300

    Add resources method to articles

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
 
   root 'articles#index'
 
-  get '/articles', to: 'articles#index'
-  get '/articles/:id', to: 'articles#show'
+  resources :articles
   # Defines the root path route ("/")
   # root "posts#index"
 end
